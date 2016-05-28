7b05fde00df2dba7c7602790acf3ad1d27b59f7b
Sergio <sergiokv13@gmail.com>
2016-05-28 06:44:26 -0400

    asd

---
+++ app/views/announcements/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @announcement, :id, :subject, :content, :user_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528101629) do
+ActiveRecord::Schema.define(version: 20160528103452) do
+
+  create_table "announcements", force: :cascade do |t|
+    t.string   "subject"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "campaments", force: :cascade do |t|
     t.string   "name"
