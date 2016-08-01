ad2a344f3208454b0b227685dd4753014138b04e
alexgunner <aptpta@yahoo.es>
2016-08-01 12:23:29 -0400

    Arreglo en capitulos

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -7,7 +7,7 @@ class Chapter < ActiveRecord::Base
   belongs_to :chapter_consultant_president, foreign_key: 'chapter_consultant_president_id',class_name: 'User'
   has_many :consultants, foreign_key: 'chapter_consultant_id', class_name: 'User'
   has_many :demolays, foreign_key: 'chapter_id', class_name: 'User'
-  #has_many :knights, foreign_key: 'priory_id', class_name: 'User'
+  has_many :knights, foreign_key: 'priory_id', class_name: 'User'
   has_many :chevalliers, foreign_key: 'court_id', class_name: 'User'
 
   def announcements_to_aprove
