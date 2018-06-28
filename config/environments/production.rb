c507f34c7d0e947c60076575ae3d28ecf50bed50
Sergio Köller <sergiokv13@gmail.com>
2018-06-28 12:28:34 -0400

    reset password isntructions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'sirdembo.com' }
+
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :domain               => "gmail.com",
+   :user_name            => 'eventos.dm.bolivia@gmail.com',
+   :password             => 'CongresosDM075',
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+  }
+
 end
